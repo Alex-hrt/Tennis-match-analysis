745982a2ad23d8ca3faf0450be75c3a1041d9502
Alex-hrt <alex.heritier@outlook.fr>
2024-11-07 17:17:14 +0100

    Added ball detection w/ stubs & bounding boxes

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from utils import (read_video, save_video)
-from trackers import PlayerTracker
+from trackers import PlayerTracker, BallTracker
 
 def main():
     # Split video into frames
     input_video_path = "input_videos/input_video.mp4"
     video_frames = read_video(input_video_path)
 
-    # Detecting players
+    # Detection
+    ## Detecting players
     player_tracker = PlayerTracker(model_path='yolo11x')
     player_detections = player_tracker.detect_frames(video_frames, read_from_stub=True, stub_path="tracker_stubs/player_detections.pkl")
+
+    ## Detecting balls
+    ball_tracker = BallTracker(model_path='models/yolo11x_last.pt')
+    ball_detections = ball_tracker.detect_frames(video_frames, read_from_stub=True, stub_path="tracker_stubs/ball_detections.pkl")
     
-    # Draw Player Bounding Boxes
+    # Drawing Bounding Boxes
+    ## Draw Player Bounding Boxes
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
+
+    ## Draw Ball Bounding Boxes
+    output_video_frames = ball_tracker.draw_bboxes(video_frames, ball_detections)
 
     # Combines frames to video
     save_video(output_video_frames, "output_videos/output_video.avi")
