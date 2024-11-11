a241e82cc35e0593f95120dd1d83276e9e8b2037
Alex-hrt <alex.heritier@outlook.fr>
2024-11-11 13:38:01 +0100

    Interpolation on ball position

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from utils import (read_video, save_video)
+from utils import read_video, save_video
 from trackers import PlayerTracker, BallTracker
 from court_line_detector import CourtLineDetector
+
 
 def main():
     # Split video into frames
@@ -9,18 +10,24 @@ def main():
 
     # Detection
     ## Detecting players
-    player_tracker = PlayerTracker(model_path='yolo11x')
-    player_detections = player_tracker.detect_frames(video_frames, read_from_stub=True, stub_path="tracker_stubs/player_detections.pkl")
+    player_tracker = PlayerTracker(model_path="yolo11x")
+    player_detections = player_tracker.detect_frames(
+        video_frames,
+        read_from_stub=True,
+        stub_path="tracker_stubs/player_detections.pkl",
+    )
 
-    ## Detecting balls
-    ball_tracker = BallTracker(model_path='models/yolo11x_last.pt')
-    ball_detections = ball_tracker.detect_frames(video_frames, read_from_stub=True, stub_path="tracker_stubs/ball_detections.pkl")
+    ## Detecting ball
+    ball_tracker = BallTracker(model_path="models/yolo11x_last.pt")
+    ball_detections = ball_tracker.detect_frames(
+        video_frames, read_from_stub=True, stub_path="tracker_stubs/ball_detections.pkl"
+    )
+    ball_detections = ball_tracker.interpolate_ball_positions(ball_detections)
 
     ## Detecting Court line keypoints
-    court_model_path = 'models/keypoints_model.pth'
+    court_model_path = "models/keypoints_model.pth"
     court_line_detector = CourtLineDetector(court_model_path)
     court_keypoints = court_line_detector.predict(video_frames[0])
-    
 
     # Drawing Bounding Boxes
     ## Draw Player Bounding Boxes
@@ -30,11 +37,13 @@ def main():
     output_video_frames = ball_tracker.draw_bboxes(output_video_frames, ball_detections)
 
     ## Draw Court keypoints
-    output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
-
+    output_video_frames = court_line_detector.draw_keypoints_on_video(
+        output_video_frames, court_keypoints
+    )
 
     # Combines frames to video
     save_video(output_video_frames, "output_videos/output_video.avi")
+
 
 if __name__ == "__main__":
     main()
--- trackers/ball_tracker.py
+++ trackers/ball_tracker.py
@@ -1,27 +1,28 @@
-from ultralytics import YOLO
-import cv2
 import pickle
+import cv2
+import pandas as pd
+from ultralytics import YOLO
+
 
 class BallTracker:
-    def __init__(self,model_path):
+    def __init__(self, model_path):
         self.model = YOLO(model_path)
-    
+
     # Processes a list of frames to detect balls and returns a list of ball detections, while saving/reading to/from a stub file
     def detect_frames(self, frames, read_from_stub=False, stub_path=None):
-        
         ball_detections = []
 
         if read_from_stub and stub_path is not None:
-            with open(stub_path, 'rb') as f:
+            with open(stub_path, "rb") as f:
                 ball_detections = pickle.load(f)
             return ball_detections
 
         for frame in frames:
             ball_dict = self.detect_frame(frame)
             ball_detections.append(ball_dict)
-        
+
         if stub_path is not None:
-            with open(stub_path, 'wb') as f:
+            with open(stub_path, "wb") as f:
                 pickle.dump(ball_detections, f)
 
         return ball_detections
@@ -34,9 +35,9 @@ class BallTracker:
         for box in results.boxes:
             result = box.xyxy.tolist()[0]
             ball_dict[1] = result
-        
+
         return ball_dict
-    
+
     # Takes video frames and ball detections, then draws bounding boxes around balls with their IDs and returns the modified frames
     def draw_bboxes(self, video_frames, ball_detections):
         output_video_frames = []
@@ -44,8 +45,38 @@ class BallTracker:
             # Draw Bounding Boxes
             for track_id, bbox in ball_dict.items():
                 x1, y1, x2, y2 = bbox
-                cv2.putText(frame, f"Ball ID: {track_id}",(int(bbox[0]),int(bbox[1] -10 )),cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 255), 2)
-                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 255), 2)
+                cv2.putText(
+                    frame,
+                    f"Ball ID: {track_id}",
+                    (int(bbox[0]), int(bbox[1] - 10)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.9,
+                    (0, 255, 255),
+                    2,
+                )
+                cv2.rectangle(
+                    frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 255), 2
+                )
             output_video_frames.append(frame)
-        
+
         return output_video_frames
+
+    # Takes a list of ball positions, converts it into a Pandas DataFrame, interpolates missing values, and returns the interpolated ball positions
+    def interpolate_ball_positions(self, ball_positions):
+        ball_positions = [x.get(1, []) for x in ball_positions]
+
+        # Convert the list into Pandas DataFrame
+        df_ball_positions = pd.DataFrame(
+            ball_positions, columns=["x1", "y1", "x2", "y2"]
+        )
+
+        # Interpolate the missing values
+        df_ball_positions = df_ball_positions.interpolate()
+
+        # Backfill positions if no detections on first frame
+        df_ball_positions = df_ball_positions.bfill()
+
+        # Convert back into original list style
+        ball_positions = [{1: x} for x in df_ball_positions.to_numpy().tolist()]
+
+        return ball_positions
