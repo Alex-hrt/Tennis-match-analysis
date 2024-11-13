d16345bfc6feb63cba3cea5908ffdd8c706047a4
Alex-hrt <alex.heritier@outlook.fr>
2024-11-13 11:45:53 +0100

    Changed player bbox to green

--- trackers/player_tracker.py
+++ trackers/player_tracker.py
@@ -60,11 +60,11 @@ class PlayerTracker:
                     (int(bbox[0]), int(bbox[1] - 10)),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     0.9,
-                    (0, 0, 255),
+                    (0, 255, 0),
                     2,
                 )
                 cv2.rectangle(
-                    frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2
+                    frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2
                 )
             output_video_frames.append(frame)
 
